fc21934c6e0ba147a255d36ec35a98db4ddaf39c
Maksim Bazhenov <maksim4k1@gmail.com>
2021-08-13 14:55:45 +0600

    added markup and styles for FooterNavigation component

---
+++ src/components/Footer/FooterNavigation/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "./FooterNavigation.module.scss";
+
+function FooterNavigation () {
+    return(
+        <div className={style.footer__navigation}>
+            
+        </div>
+    );
+}
+
+export default FooterNavigation;
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import style from "./Footer.module.scss";
 import whatsapp from "../../img/svg/footer/whatsapp-footer-icon.svg";
+import FooterNavigation from "./FooterNavigation";
 
 function Footer() {
     return(
         <footer className={style.footer}>
             <div className={`${style.footer__container} container`}>
                 <h2 className={style.footer__logo}><a href="/">Авто Дом</a></h2>
+                <FooterNavigation/>
                 <div className={style.footer__license}>
                     <a href="https://wa.me/+996556705055" rel="noreferrer" target="_blank" className={style.footer__whatsapp}><img src={whatsapp} alt="WhatsApp" className={style.footer__icon}/></a>
                     <p className={style.footer__text}>©Авто Дом 2021</p>
