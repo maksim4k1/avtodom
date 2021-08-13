9471b709a78ca031917c1849444b4a7bfbac8fe0
Maksim Bazhenov <maksim4k1@gmail.com>
2021-08-13 11:27:38 +0600

    added markup and styles for Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,7 +4,9 @@ import style from "./Header.module.scss";
 function Header() {
     return(
         <header className={style.header}>
-            
+            <div className={`${style.header__container} container`}>
+                <h2 className={style.header__logo}><a href="/">Авто Дом</a></h2>
+            </div>
         </header>
     );
 }
