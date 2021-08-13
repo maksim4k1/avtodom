765a40b1567c9352269de11109b6c2ee8ed4a864
Maksim Bazhenov <maksim4k1@gmail.com>
2021-08-13 14:51:11 +0600

    added markup and styles for Footer component

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import style from "./Footer.module.scss";
+import whatsapp from "../../img/svg/footer/whatsapp-footer-icon.svg";
 
 function Footer() {
     return(
         <footer className={style.footer}>
-            
+            <div className={`${style.footer__container} container`}>
+                <h2 className={style.footer__logo}><a href="/">Авто Дом</a></h2>
+                <div className={style.footer__license}>
+                    <a href="https://wa.me/+996556705055" rel="noreferrer" target="_blank" className={style.footer__whatsapp}><img src={whatsapp} alt="WhatsApp" className={style.footer__icon}/></a>
+                    <p className={style.footer__text}>©Авто Дом 2021</p>
+                </div>
+            </div>
         </footer>
     );
 }
